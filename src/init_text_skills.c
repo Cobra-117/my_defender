1df35e45fa26cde6772e668343184f24f9887bfb
Adrien Michaud <CubeLeopard5@localhost.localdomain>
2020-02-28 05:49:51 -0500

    change the text of the lvl2 turrets

--- src/init_text_skills.c
+++ src/init_text_skills.c
@@ -10,11 +10,11 @@
 void init_text_tesla2(Index_t *index)
 {
     index->text_skills.txt_tesla2 = sfText_create();
-    sfText_setString(index->text_skills.txt_tesla2, "Tesla lvl2");
+    sfText_setString(index->text_skills.txt_tesla2, "Tazer lvl2");
     sfText_setFont(index->text_skills.txt_tesla2, index->ui_text.font2);
     sfText_setColor(index->text_skills.txt_tesla2, sfWhite);
     sfText_setCharacterSize(index->text_skills.txt_tesla2, 22);
-    index->text_skills.pos_tesla2.x = 1160;
+    index->text_skills.pos_tesla2.x = 640;
     index->text_skills.pos_tesla2.y = 190;
     sfText_setPosition(index->text_skills.txt_tesla2, index->
     text_skills.pos_tesla2);
@@ -24,11 +24,11 @@ void init_text_tesla2(Index_t *index)
 void init_text_flame2(Index_t *index)
 {
     index->text_skills.txt_flame2 = sfText_create();
-    sfText_setString(index->text_skills.txt_flame2, "Flame lvl2");
+    sfText_setString(index->text_skills.txt_flame2, "Pussyfeu lvl2");
     sfText_setFont(index->text_skills.txt_flame2, index->ui_text.font2);
     sfText_setColor(index->text_skills.txt_flame2, sfWhite);
     sfText_setCharacterSize(index->text_skills.txt_flame2, 22);
-    index->text_skills.pos_flame2.x = 1030;
+    index->text_skills.pos_flame2.x = 640;
     index->text_skills.pos_flame2.y = 190;
     sfText_setPosition(index->text_skills.txt_flame2, index->
     text_skills.pos_flame2);
@@ -38,11 +38,11 @@ void init_text_flame2(Index_t *index)
 void init_text_arti2(Index_t *index)
 {
     index->text_skills.txt_arti2 = sfText_create();
-    sfText_setString(index->text_skills.txt_arti2, "Arti lvl2");
+    sfText_setString(index->text_skills.txt_arti2, "Artillery lvl2");
     sfText_setFont(index->text_skills.txt_arti2, index->ui_text.font2);
     sfText_setColor(index->text_skills.txt_arti2, sfWhite);
     sfText_setCharacterSize(index->text_skills.txt_arti2, 22);
-    index->text_skills.pos_arti2.x = 900;
+    index->text_skills.pos_arti2.x = 640;
     index->text_skills.pos_arti2.y = 190;
     sfText_setPosition(index->text_skills.txt_arti2, index->
     text_skills.pos_arti2);
@@ -52,11 +52,11 @@ void init_text_arti2(Index_t *index)
 void init_text_heavy2(Index_t *index)
 {
     index->text_skills.txt_heavy2 = sfText_create();
-    sfText_setString(index->text_skills.txt_heavy2, "Heavy lvl2");
+    sfText_setString(index->text_skills.txt_heavy2, "Big Borris lvl2");
     sfText_setFont(index->text_skills.txt_heavy2, index->ui_text.font2);
     sfText_setColor(index->text_skills.txt_heavy2, sfWhite);
     sfText_setCharacterSize(index->text_skills.txt_heavy2, 22);
-    index->text_skills.pos_heavy2.x = 770;
+    index->text_skills.pos_heavy2.x = 640;
     index->text_skills.pos_heavy2.y = 190;
     sfText_setPosition(index->text_skills.txt_heavy2, index->
     text_skills.pos_heavy2);
@@ -66,7 +66,7 @@ void init_text_heavy2(Index_t *index)
 void init_text_casu2(Index_t *index)
 {
     index->text_skills.txt_casu2 = sfText_create();
-    sfText_setString(index->text_skills.txt_casu2, "Casu lvl2");
+    sfText_setString(index->text_skills.txt_casu2, "Popular turret lvl2");
     sfText_setFont(index->text_skills.txt_casu2, index->ui_text.font2);
     sfText_setColor(index->text_skills.txt_casu2, sfWhite);
     sfText_setCharacterSize(index->text_skills.txt_casu2, 22);
