fce0af672350eee7dde14215c2eadece2a4ff0f0
Adrien Michaud <CubeLeopard5@localhost.localdomain>
2020-03-05 05:08:48 -0500

    try a debug

--- src/game_loop.c
+++ src/game_loop.c
@@ -70,7 +70,6 @@ int game_main_function(Index_t *index)
 int game_loop(Index_t *index)
 {
     quick_init(index);
-    init_price_turrets(index);
     init_user_interface(index);
     while (game_main_function(index) != 1 && index->what_is_open != 5
     && index->what_is_open != 6);
